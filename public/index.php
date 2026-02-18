ee93b3c2e170d78f0c6e143ef53d659ff6247266
VanessaFAG <vanessaprincss.arreo@gmail.com>
2026-02-17 18:40:06 -0600

    Full Corrección

--- public/index.php
+++ public/index.php
@@ -6,8 +6,9 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 require_once '../core/Router.php';
 require_once '../resources/v1/userResource.php';
-require_once '../resources/v1/productResourse.php';
+require_once '../resources/v1/productResource.php';
 require_once '../resources/v1/authResource.php';
+require_once '../models/auth.php';
 
 $scriptName = dirname($_SERVER['SCRIPT_NAME']);
 $basePath = $scriptName;
@@ -16,6 +17,31 @@ $router = new Router('v1', $basePath);
 $userResource = new UserResource();
 $productResource =  new ProductResource();
 $authResource = new AuthResource();
+
+// Rutas publicas
+$router->addRoute('POST', '/login', [$authResource, 'login']);
+
+// Logica de protección
+$currenUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if(strpos($currenUri, '/login') === false){
+    $headers = getallheaders();
+    $token = null;
+
+    if (isset($headers['Authorization'])){
+        $token = str_replace('Bearer ', '', $headers['Autorization']);
+    }
+
+
+    $db = (new Database())->getConnection();
+    $auth = new Auth($db);
+
+    if(!$token || !$auth->validateToken($token)){
+        http_response_code(401);
+        echo json_encode(["message" => "Acceso no autorizado. Token invalido o innexistente."]);
+    exit();
+    }
+}
 
 // rutas
 $router->addRoute('GET', '/users', [$userResource, 'index']);
@@ -26,15 +52,6 @@ $router->addRoute('DELETE', '/users/{id}', [$userResource, 'destroy']);
 
 $router->addRoute('GET', '/productos', [$productResource, 'index']);
 $router->addRoute('GET', '/productos/{id}', [$productResource, 'show']);
-$router->addRoute('POST', '/productos', [$productResource, 'store']);
-$router->addRoute('PUT', '/productos/{id}', [$productResource, 'update']);
-$router->addRoute('DELETE', '/productos/{id}', [$productResource, 'destroy']);
 
-
-$router->addRoute('GET', '/login', [$$authResource, 'index']);
-$router->addRoute('GET', '/login/{id}', [$$authResource, 'show']);
-$router->addRoute('POST', '/login', [$$authResource, 'store']);
-$router->addRoute('PUT', '/login/{id}', [$$authResource, 'update']);
-$router->addRoute('DELETE', '/login/{id}', [$$authResource, 'destroy']);
 $router->dispatch();
 ?>
